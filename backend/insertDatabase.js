eddabe1de6b638bec93946d10f7c653aa7dd82e1
Niklas <niklas_loos@web.de>
2023-07-20 14:02:49 +0200

    delete no need code

--- backend/insertDatabase.js
+++ backend/insertDatabase.js
@@ -1,29 +1,6 @@
 const http = require('http');
 const db = require('./databaseConnection.js');
 
-const server = http.createServer((req, res) => {
-  // Endpunkt-Handling
-  if (req.method === 'POST' && req.url === 'http://localhost:3000/register') {
-    console.log('Verbindung geklappt');
-  } else {
-    // Endpunkt nicht gefunden
-    console.log('schief gelaufen');
-    res.statusCode = 404;
-    res.end();
-  }
-});
-
-server.listen(3000, () => {
-  console.log('Server läuft auf Port 3000');
-});
-
-function handleRegister(res) {
-  // Hier kannst du den Anforderungs- und Antwortcode für den '/register'-Endpunkt einfügen
-  // Beispiel: Daten empfangen, in die Datenbank einfügen, usw.
-  databaseFunctions.insertData(); // Beispiel für die Verwendung einer Funktion aus databaseFunctions
-  res.statusCode = 200;
-  res.end('Registrierung erfolgreich!');
-}
 
 const databaseFunctions = {
   connectToDatabase() {
@@ -65,4 +42,3 @@ const databaseFunctions = {
   }
 };
 
-// Server starten
