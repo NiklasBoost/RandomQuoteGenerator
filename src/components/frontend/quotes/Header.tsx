fc6a2c64c071fd64aeef7efcc6b4c9a625e1e80a
Niklas <niklas_loos@web.de>
2023-09-18 19:21:32 +0200

    transform in react

---
+++ src/components/frontend/quotes/Header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <div className="header">
+      <PlaceholerHeader />
+
+    </div>
+  )
+}
+
+function PlaceholerHeader() {
+  return <div className="placeholder-header"></div>;
+}
