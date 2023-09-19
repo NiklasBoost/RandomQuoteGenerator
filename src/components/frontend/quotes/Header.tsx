59e9bba0fbe0fd3674e2d494e3e870a8f01c7f83
Niklas <niklas_loos@web.de>
2023-09-19 07:14:29 +0200

    copy css styles in react

--- src/components/frontend/quotes/Header.tsx
+++ src/components/frontend/quotes/Header.tsx
@@ -2,11 +2,30 @@ export function Header() {
   return (
     <div className="header">
       <PlaceholerHeader />
-
+      <Searchbar />
+      <Edit />
     </div>
   )
 }
 
 function PlaceholerHeader() {
   return <div className="placeholder-header"></div>;
+}
+
+function Searchbar() {
+  return (
+    <div className="searchbar-container">
+      <input className="searchbar" placeholder="search your quote - press enter for search!" />
+    </div>
+  );
+}
+
+function Edit() {
+  return (
+    <div className="edit-container">
+      <button className="edit-button-js edit-button">
+        edit
+      </button>
+    </div>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Header } from './components/frontend/quotes/header'
+import { Header } from './components/frontend/quotes/Header.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+ 
   <React.StrictMode>
     <Header />
   </React.StrictMode>,
