bd0553f0736c0f502fc9981345ecd58b6bcc455e
Niklas <niklas_loos@web.de>
2023-09-21 20:40:21 +0200

    remove dictionary

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Header } from './components/frontend/quotes/Header.tsx'
-import { MiddlePart } from './components/frontend/quotes/MiddlePart.tsx'
-import { Footer } from './components/frontend/quotes/Footer.tsx'
+import { Header } from './components/quotes/Header.tsx'
+import { MiddlePart } from './components/quotes/MiddlePart.tsx'
+import { Footer } from './components/quotes/Footer.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
