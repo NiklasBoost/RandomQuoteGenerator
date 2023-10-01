b65862bea659578b875f0baef3a17536c5c2b3eb
Niklas <niklas_loos@web.de>
2023-10-01 13:15:00 +0200

    add fav code design

--- src/App-Test.tsx
+++ src/App-Test.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Container, Row, Col, Button, InputGroup, FormControl, Form } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog, faArrowRight, faArrowLeft, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faArrowRight, faArrowLeft, faPlus, faHeart, faHeartBroken } from '@fortawesome/free-solid-svg-icons';
 
  function AppTest() {
   const [isEditing, setIsEditing] = React.useState(false);
@@ -15,60 +15,58 @@ import { faCog, faArrowRight, faArrowLeft, faPlus } from '@fortawesome/free-soli
     <Container fluid style={{ minHeight: '100vh'}}>
       <Row className="mt-4">
         <Col>
-          <Button variant="primary">Deine Zitate</Button>
-          <Button className='mt-1' variant="primary">Deine Favoriten</Button>
+          <Button className='mx-1' variant="primary">Deine Zitate</Button>
+          <Button variant="primary">Deine Favoriten</Button>
         </Col>
-        <Col xs={6}>
+        <Col md={6}>
           <InputGroup>
-            <FormControl placeholder="Suche nach Zitaten" />
+            <FormControl placeholder="Suche nach Zitaten - einfach Enter drücken" />
           </InputGroup>
         </Col>
         <Col>
-          <div>
-            <Button variant="secondary">
-              {isEditing ? 'Speichern' : 'Bearbeiten'}
-            </Button>
-            <div>{feedbackDom}</div>
-          </div>
-        </Col>
-        <Col>
-          <Button variant="primary">
-            <FontAwesomeIcon icon={faCog} />
+          <Button className='position-absolute top-0 end-0 p-4' variant='none'>
+            <FontAwesomeIcon icon={faCog} size='2x' />
           </Button>
         </Col>
       </Row>
-      <Row className="mt-5">
-        <Col xs={1}>
+      <Row className="mx-1 mt-5 py-5" style={{ background: '#f5f5f5' }}>
+        <Col md={1}>
           <Button variant="light">
             <FontAwesomeIcon icon={faArrowLeft} />
           </Button>
         </Col>
-        <Col xs={6}>
+        <Col md={6}>
           <div>Das ist nur ein langes Testzitat, um zu sehen, wie sich die Seite verhält</div>
           <div>- von mir</div>
           <div>
-            <Form.Check
-              type="checkbox"
-              label="Favorit?"
-            />
+          <span >
+            <FontAwesomeIcon 
+              className='mt-3 text-danger'
+              icon={faHeartBroken}
+              size='2x' />
+          </span>
           </div>
         </Col>
-        <Col xs={1}>
+        <Col md={1}>
           <Button variant="light">
             <FontAwesomeIcon icon={faArrowRight}/>
           </Button>
         </Col>
-        <Col>
-          <Button variant="danger">Entfernen</Button>
+        <Col className='mx-2' md={1}>
+          <Button variant="secondary">
+            {isEditing ? 'Speichern' : 'Bearbeiten'}
+          </Button>
+          <div>FeedbackDiv</div>
         </Col>
-        <Col>
-          <div>{feedbackDom}</div>
+        <Col md={1}>
+          <Button variant="danger">Entfernen</Button>
+          <div>FeedbackDiv</div>
         </Col>
       </Row>
       <Row className="px-3 py-3 fixed-bottom">
         <Col>
-          <div>{feedback}</div>
-          <InputGroup>
+          <div>FeedbackDiv</div>
+          <InputGroup className='mt-2'>
             <FormControl placeholder="Zitat" />
             <FormControl placeholder="Autor" />
             <Button variant="success">
