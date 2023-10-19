c1669af9e8dc742189bd45a97c0ba446087ea3fb
Niklas <niklas_loos@web.de>
2023-10-19 19:39:37 +0200

    remove Test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AppTest from "./App-Test";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    {/* <AppTest /> */}
   </React.StrictMode>
 );
